6601aeda021f495e5a18046583debe1ae906e732
esa606 <dzunkh88@yahoo.com>
2015-03-16 14:34:33 -0500

    create table hungarian_base.sql

---
+++ create table hungarian_base.sql
@@ -0,0 +1,11 @@
+create table hungarian_base (
+  elt_idx        integer,
+  row_idx        integer,
+  col_idx        integer,
+  price          integer,
+  starred        integer,
+  row_covered    integer,
+  col_covered    integer,
+  prime_sequence integer,
+  z_sequence     integer
+);

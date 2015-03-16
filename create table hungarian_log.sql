342f3e6db195ce915b5077999e250605c974f1f3
esa606 <dzunkh88@yahoo.com>
2015-03-16 14:34:00 -0500

    create table hungarian_log.sql

---
+++ create table hungarian_log.sql
@@ -0,0 +1,12 @@
+create table hungarian_log (
+  call_no integer,
+  step_end_time timestamp,
+  curr_step integer,
+  next_step integer,
+  sum_starred integer,
+  sum_row_covered integer,
+  sum_col_covered integer,
+  sum_prime_seq integer,
+  sum_z_seq integer,
+  n integer
+);
